d67a40a84495832335b9a537274c5b0d02a9c4da
Nuwan Jaliyagoda <NuwanJ@users.noreply.github.com>
2022-03-13 17:18:03 +0530

    Implementation for most popular project list in the home page (#86)
    * site build using GitHub Actions
    
    * Dynamic data updated
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * Dynamic data updated
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * Dynamic data updated
    
    * Merge with base (#27)
    
    * Dynamic data updated
    
    * site build using GitHub Actions
    
    * Dynamic data updated
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * Update History-of-Music.md
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * Dynamic data updated
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * site build using GitHub Actions
    
    * Dynamic data updated
    
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Akila Karunanayake <46198141+Akilax0@users.noreply.github.com>
    
    * Implementation for most popular project list in the home page
    
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Akila Karunanayake <46198141+Akilax0@users.noreply.github.com>

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,3 +18,8 @@ $(document).ready(function () {
     return false;
   });
 });
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
---
+++ python_scripts/projects_popular.py
@@ -0,0 +1,46 @@
+import requests
+import os
+import json
+from operator import itemgetter
+import random
+
+url = '../_data/projects.json'
+with open(url, 'r') as f:
+    projects = json.load(f)
+
+filtered_list = []
+
+# Intermediate list
+for p in projects:
+    updated = projects[p]['updated_at']
+    projects[p]['name'] = p
+    stars = projects[p]['stars_count']
+    watchers = projects[p]['watchers_count']
+    forks = projects[p]['forks_count']
+
+    # Calculate the score based on weights, and round to 3 digits
+    score = round(stars*0.5 + watchers*0.3 + forks*0.2, 3)
+    projects[p]['popularity_score'] = float(score)
+
+    # Only consider the projects with positive scores
+    if(score>0):
+        filtered_list.append(projects[p])
+
+# Sort as the highest scored projects first
+sorted_list = sorted(filtered_list, key=itemgetter('popularity_score'), reverse=True)
+
+# Only latest x projects will be saved here
+TOP_PROJ_LIMIT = 8
+sliced_list = sorted_list[:TOP_PROJ_LIMIT]
+
+# Suffel the projects
+random.shuffle(sliced_list)
+
+# Prepare a dictionary with the project name as the key
+result = { sliced_list[i]['name'] : sliced_list[i] for i in range(0, len(sliced_list) ) }
+
+# Write project data into file
+filename = "../_data/projects_popular.json"
+os.makedirs(os.path.dirname(filename), exist_ok=True)
+with open(filename, "w") as f:
+    f.write(json.dumps(result, indent = 4))
