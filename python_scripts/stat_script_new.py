ea328d916f3c4c20547795333b326524a1ff5e5d
Nuwan Jaliyagoda <nuwanjaliyagoda@gmail.com>
2023-06-14 20:12:20 +0530

    Script update

--- python_scripts/stat_script_new.py
+++ python_scripts/stat_script_new.py
@@ -39,160 +39,72 @@ RESULTS_PER_PAGE = 100
 projects = []
 
 
-def urlOrganization():
-    return "https://api.github.com/orgs/{}".format(ORGANIZATION)
-
-
-def urlOrganizationRepos(pageNo):
-    return "https://api.github.com/orgs/{}/repos?page={}".format(ORGANIZATION, pageNo)
-
-
-def inRange(x, minNumber, maxNumber):
-    if type(x) == str:
-        x = int(x)
-    if minNumber > maxNumber:
-        minNumber, maxNumber = maxNumber, minNumber
-    if minNumber <= x and maxNumber >= x:
-        return True
-    else:
-        return False
-
-
-def writeHeader(category, batch, grand_parent, permalink, title, description, stars, forks, watch, date, repo, page):
-    data = {
-        'layout': "project_page",
-        'title': title,
-        'permalink': permalink,
-        'description': description,
-        'has_children': False,
-        'parent': "{} {}".format(batch.upper(), grand_parent),
-        'grand_parent': grand_parent,
-        'cover_url': "/data/categories/{}/cover_page.jpg".format(category),
-        'thumbnail_url': "/data/categories/{}/thumbnail.jpg".format(category),
-        'repo_url': repo,
-        'page_url': page,
-        'forks': str(forks),
-        'watchers': str(watch),
-        'stars': str(stars),
-        'started_on': date
-    }
-    desc = description.replace("\"", "'")
-    return "---\n{}\n---\n\n{}\n\n".format(yaml.dump(data, sort_keys=False), desc)
-
-
-def batch_index_template(code, batch, tag, project, description, readmore_link='#'):
-    data = {
-        'layout': "project_batch",
-        'title': "E{} {}".format(batch, project),
-        'permalink': "/{}/e{}/".format(tag, batch),
-        'has_children': True,
-        'parent': project,
-        'batch': "e{}".foramt(batch),
-        'code': code,
-        'readmore': readmore_link,
-        'default_thumb_image': "/data/categories/{}/{}".format(code, thumbnail),
-        'description': description,
-        'search_exclude': True
-    }
-    return yaml.dump(data, sort_keys=False)
-
-
-def index_template(id, code, title, cover, thumbnail, description, contact, proj_type, readmore_link="#"):
-    data = {
-        'layout': "project_cat",
-        'title': title,
-        'nav_order': str(id),
-        'permalink': "/{}/".format(code),
-        'has_children': True,
-        'code': code,
-        'type': proj_type,
-        'parent': "Home",
-        'has_toc': True,
-        'search_exclude': True,
-        'readmore': readmore_link,
-        'default_thumb_image': "/data/categories/{}/{}".format(code, thumbnail),
-        'description': description,
-    }
-    return yaml.dump(data, sort_keys=False)
-
-
-def md_file_write(CATEGORIES, BATCHES):
-    for code in CATEGORIES:
-        index = open("../data/categories/{}/index.json".fomat(i), 'r')
-        index_data = index.read()
-        data = json.loads(index_data)
-
-        description = data['description']
+def md_file_write(CATEGORIES, batch_map):
+    id = 0
+    for code in batch_map:
+        data = CATEGORIES[code]
         readmore_link = data['readmore'] if ('readmore' in data) else '#'
 
-        for batch in BATCHES[code]:
-            # batch_str = str(batch)
-            # if batch < 10:
-            #     batch_str = '0' + batch_str
-            filename = "e{:02}".foramt(batch)
+        index_file = {
+            'layout': "project_cat",
+            'title': data['title'],
+            'nav_order': str(id),
+            'permalink': "/{}/".format(code),
+            'has_children': True,
+            'code': data['code'],
+            'type': data['type'],
+            'parent': "Home",
+            'has_toc': True,
+            'search_exclude': True,
+            'readmore': readmore_link,
+            'default_thumb_image': "/data/categories/{}/{}".format(data['code'], data['images']['thumbnail']),
+            'description': data['description'],
+        }
 
-            outputFile = open(path, "w+")
-            # code, batch, tag, project, description, readmore_link='#'
-            # outputFile.write(batch_index_template(code=str(i), batch=batch_str, tag=str(i), CATEGORIES[i], description, readmore_link))
+        id += 1
+        path = "../categories/" + str(code) + "/index.md"
+        os.makedirs(os.path.dirname(path), exist_ok=True)
 
-            project = CATEGORIES[code]
-            data = {
+        print(">> Write category index for", code)
+        with open(path, "w") as f:
+            f.write('---\n')
+            f.write(yaml.dump(index_file, sort_keys=False))
+            f.write('---\n')
+
+        for batch in batch_map[code]:
+            batch_file = {
                 'layout': "project_batch",
-                'title': "E{} {}".format(batch, project),
-                'permalink': "/{}/e{}/".format(code, batch),
+                'title': "E{} {}".format(batch[1:], CATEGORIES[code]['title']),
+                'permalink': "/{}/{}/".format(code, batch),
                 'has_children': True,
-                'parent': project,
-                'batch': "e{:02}".foramt(batch),
+                'parent': CATEGORIES[code]['title'],
+                'batch': batch,
                 'code': str(code),
                 'readmore': readmore_link,
-                'default_thumb_image': "/data/categories/{}/{}".format(code, thumbnail),
-                'description': description,
-                'search_exclude': True
+                'search_exclude': True,
+                'default_thumb_image': "/data/categories/{}/{}".format(code, data['images']['thumbnail']),
+                'description': data['description'],
             }
 
-            path = "../categories/{}/{}.md".format(code, filename)
+            path = "../categories/{}/{}.md".format(code, batch)
             os.makedirs(os.path.dirname(path), exist_ok=True)
-            with open(path, "w") as f:
-                f.write('---')
-                f.write(yaml.dump(data, sort_keys=False))
-                f.write('---')
+
+            print("\t", batch)
+            with open(path, "w") as f2:
+                f2.write('---\n')
+                f2.write(yaml.dump(batch_file, sort_keys=False))
+                f2.write('---\n')
 
 
-def index_files(CATEGORIES):
-    id = 0
-    for i in CATEGORIES:
-        index = open("../data/categories/" + str(i) + "/index.json", 'r')
-        index_data = index.read()
-        data = json.loads(index_data)
-
-        code = data['code']
-        title = data['title']
-        cover = data['images']['cover']
-        thumbnail = data['images']['thumbnail']
-        description = data['description']
-        contact = data['contact']
-
-        # course project or general
-        categoryType = data['type']
-        readmore_link = data['readmore'] if ('readmore' in data) else '#'
-
-        path = "../categories/" + str(i) + "/index.md"
-        os.makedirs(os.path.dirname(path), exist_ok=True)
-        outputFile = open(path, "w+")
-        id += 1
-        outputFile.write(index_template(id, code, title, cover,
-                         thumbnail, description, contact, categoryType, readmore_link))
-
-
-def del_docs_categories():
+def del_old_files():
+    # Delete category index files
     dir_path = "../categories/"
     try:
         shutil.rmtree(dir_path)
     except OSError as e:
         print("Error: %s : %s" % (dir_path, e.strerror))
 
-
-def del_docs_github_repos():
+    # Delete project files
     dir_path = "../projects/github_projects/"
     try:
         shutil.rmtree(dir_path)
@@ -202,7 +114,7 @@ def del_docs_github_repos():
 
 print("START")
 
-del_docs_github_repos()
+del_old_files()
 
 # # Download the repository data
 # repo_list = []
@@ -237,7 +149,7 @@ for r in repo_list:
         r_name = r['name'].strip().split('-')
 
         if r_name[0][0] == 'e' and r_name[0][1:].isdigit() and len(r_name) > 2:
-            batch = "{:02}".format(int(r_name[0][1:]))
+            batch = "e{:02}".format(int(r_name[0][1:]))
             cat = r_name[1].lower()
             title = ' '.join(r_name[2:])
             filename = '-'.join(r_name[2:])
@@ -252,7 +164,7 @@ for r in repo_list:
                 gh_page = "https://cepdnaclk.github.io/{}".format(
                     r['name']) if r["has_pages"] else 'blank'
 
-                path = "../projects/github_projects/{}/e{}/{}.md".format(
+                path = "../projects/github_projects/{}/{}/{}.md".format(
                     cat, batch, filename)
 
                 desc = r["description"].strip().replace(
@@ -263,7 +175,7 @@ for r in repo_list:
                 data = {
                     'layout': "project_page",
                     'title': title,
-                    'permalink': "/{}/e{}/{}/".format(cat, batch, filename),
+                    'permalink': "/{}/{}/{}/".format(cat, batch, filename),
                     'description': str(desc),
                     'has_children': False,
                     'parent': "E{} {}".format(batch.upper(), cat_name),
@@ -285,15 +197,21 @@ for r in repo_list:
                     f.write("---\n\n")
                     f.write(description)
 
-            # TODO: Update batch/cat files
+                # TODO: Update batch/cat files
+                BATCHES[cat].add(batch)
 
             else:
                 # TODO: Handle error
-                print("not belonged to a category",  r['name'])
+                print(">> Error: not belonged to a category",  r['name'])
 
     except Exception as e:
         # TODO: Notify in the Discord Channel with message
-        print(">> An exception occurred with:", r["name"], e)
+        a = 1
+        # print(">> An exception occurred with:", r["name"], e)
 
-print(">> downloaded {} repositories".format(count))
+print(">> Downloaded {} repositories".format(count))
+
+print(BATCHES)
+md_file_write(CATEGORIES, BATCHES)
+
 print("END")
